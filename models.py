029fbd71c511fc18870c5f219ac508da67dbd6b7
Delsyke <wasykepaul@gmail.com>
2022-08-27 13:29:36 +0300

    modified models and relationships

--- models.py
+++ models.py
@@ -1,63 +1,53 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-
-
+db=SQLAlchemy()
 #----------------------------------------------------------------------------#
-# Models -------> implemented in models.py
+# Models
 #----------------------------------------------------------------------------#
 
 # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
+class Artist(db.Model):
+    __tablename__ = 'artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False, nullable=False)
+    seeking_description=db.Column(db.String(120))
+    shows = db.relationship('Show', backref='artist')
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-
-class ContactInfo(db.Model):
-    __tablename__='ContactInfo'
-
-    id = db.Column(db.Integer, primary_key = True)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False, nullable=False)
+    seeking_description=db.Column(db.String(120))
+    shows = db.relationship('Show', backref='venue')
     website = db.Column(db.String(120))
-
+    facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    
 
 class Show(db.Model):
-    __tablename__= 'Show'
+    __tablename__= 'show'
 
     id = db.Column(db.Integer, primary_key = True)
     start_time = db.Column(db.DateTime)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-
-
-
-db.create_all()
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
