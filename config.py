169bbd5b531995f6fb362b9aaf06442f9478f6a0
Delsyke <wasykepaul@gmail.com>
2022-08-27 13:25:13 +0300

    set configuration for app and database

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 import os
-SECRET_KEY = os.urandom(32)
+from dotenv import load_dotenv
+#SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 # Enable debug mode.
-DEBUG = True
-TRACK_MODIFICATIONS = False
 # Connect to the database
-
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:yyyyy@localhost:5432/test'
+
+
+class Config:
+    load_dotenv()
+    password = os.environ['PASSWORD']
+    SECRET_KEY = os.urandom(32)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:{password}@localhost:5432/fyur'
+    SESSION_TYPE = "filesystem"
+    SESSION_PERMANENT = False
+    DEBUG = True
